e0df945c7d1bbf4de8926f88a34fb0bfd38596a0
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2023-01-26 14:31:17 -0300

    home produtos

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php get_header();?>
-    <?php get_template_part('template-parts/banner-home-top') ;?>
+    <?php get_template_part('template-parts/home-banner-top') ;?>
     <?php get_template_part('template-parts/home-produto-destaque') ;?>
+    <?php get_template_part('template-parts/home-products') ;?>
 <?php get_footer();?>
---
+++ template-parts/home-products.php
@@ -0,0 +1,49 @@
+<section class="products">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 d-flex align-items-center">
+                <div class="content">
+                    <h3 class="h3title">NOSSA</h3>
+                    <h2 class="h2title">BOUTIQUE</h2>
+                    <p class="description mt-2">Confira nossas ofertas em:</p>
+                    <p class="itens">- Peças</p>
+                    <p class="itens">- Acessórios</p>
+                    <p class="itens">- Vestuário</p>
+                    <div class="btn know-more mt-2">SAIBA MAIS</div>
+                </div>
+            </div>
+            <div class="col-6">
+                <img class="img-itens" src="http://casarithemeinstitucional.local/wp-content/uploads/2023/01/produto-boutique.png">  
+            </div>
+            <div class="col-6">
+                <img class="img-itens" src="http://casarithemeinstitucional.local/wp-content/uploads/2023/01/produto-boutique1.png">  
+            </div>
+            <div class="col-6 d-flex align-items-center">
+                <div class="content">
+                    <h3 class="h3title">NOSSA</h3>
+                    <h2 class="h2title">BOUTIQUE</h2>
+                    <p class="description mt-2">Confira nossas ofertas em:</p>
+                    <p class="itens">- Peças</p>
+                    <p class="itens">- Acessórios</p>
+                    <p class="itens">- Vestuário</p>
+                    <div class="btn know-more mt-2">SAIBA MAIS</div>
+                </div>
+            </div>
+            <div class="col-6 d-flex align-items-center">
+                <div class="content">
+                    <h3 class="h3title">NOSSA</h3>
+                    <h2 class="h2title">BOUTIQUE</h2>
+                    <p class="description mt-2">Confira nossas ofertas em:</p>
+                    <p class="itens">- Peças</p>
+                    <p class="itens">- Acessórios</p>
+                    <p class="itens">- Vestuário</p>
+                    <div class="btn know-more mt-2">SAIBA MAIS</div>
+                </div>
+            </div>
+            <div class="col-6">
+                <img class="img-itens" src="http://casarithemeinstitucional.local/wp-content/uploads/2023/01/produto-boutique2.png">  
+            </div>
+            
+        </div>
+    </div>
+</section>
